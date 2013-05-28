37bfd3e7e7e20a04af0b02cb3d8b9d66970159d9
dimka <dmk@coalla.ru>
2013-05-28 12:06:02 +0400

    .

--- TestProject/TPViewController.m
+++ TestProject/TPViewController.m
@@ -14,14 +14,62 @@
 
 @implementation TPViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    NSDictionary *dictionary = [TPViewController viewInfo:self.view];
+    NSLog(@"%@", [TPViewController jsonViewInfo:self.view]);
+}
+
+
++ (NSDictionary *)viewInfo:(UIView *)view {
+    NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
+    NSArray *properties = @[@"frame"];
+    for (NSUInteger i = 0; i < properties.count; i++) {
+        NSString *key = properties[i];
+//        id value = [view performSelector:NSSelectorFromString(key)];
+        CGRect value = [view frame];
+        [dictionary setValue:[NSValue valueWithCGRect:value] forKey:key];
+    }
+    NSMutableArray *subviews = [NSMutableArray array];
+    for (NSUInteger i = 0; i < view.subviews.count; i++) {
+        [subviews addObject:[self viewInfo:[view.subviews objectAtIndex:i]]];
+    }
+    [dictionary setValue:subviews forKey:@"subviews"];
+    return dictionary;
+}
+
++ (NSMutableString *)jsonViewInfo:(UIView *)view {
+    NSMutableString *string = [@"{" mutableCopy];
+    NSArray *properties = @[@"frame"];
+    for (NSUInteger i = 0; i < properties.count; i++) {
+        NSString *key = @"frame";
+        NSString *value = NSStringFromCGRect(view.frame);
+
+        [string appendFormat:@"\"%@\":\"%@\"", key, value];
+        if (i != properties.count - 1 || view.subviews.count > 0) {
+            [string appendString:@","];
+        }
+    }
+    if (view.subviews.count > 0) {
+        [string appendString:@"\"subviews\":["];
+        for (NSUInteger i = 0; i < view.subviews.count; i++) {
+            [string appendString:[self jsonViewInfo:[view.subviews objectAtIndex:i]]];
+            if (i != view.subviews.count - 1) {
+                [string appendString:@","];
+            }
+        }
+        [string appendString:@"]"];
+    }
+    [string appendString:@"}"];
+    return string;
+}
+
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
