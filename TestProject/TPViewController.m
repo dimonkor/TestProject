6dfcec23c9e6755a01e39590e4cdb5a3d595c788
dkorneev <dk@coalla.ru>
2013-05-28 14:40:10 +0400

    ...

--- TestProject/TPViewController.m
+++ TestProject/TPViewController.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2013 Дмитрий Коротченков. All rights reserved.
 //
 
+#import <CoreGraphics/CoreGraphics.h>
 #import "TPViewController.h"
 
 @interface TPViewController ()
@@ -21,44 +22,69 @@
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
-    NSDictionary *dictionary = [TPViewController viewInfo:self.view];
-    NSLog(@"%@", [TPViewController jsonViewInfo:self.view]);
+    NSArray *params = @[@"backgroundColor", @"frame", @"opaque", @"alpha", @"hidden", @"opaque", @"clipsToBounds", @"tag"];
+    NSLog(@"%@", [TPViewController JSONDescription:self.view params:params]);
 }
 
-
-+ (NSDictionary *)viewInfo:(UIView *)view {
-    NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
-    NSArray *properties = @[@"frame"];
-    for (NSUInteger i = 0; i < properties.count; i++) {
-        NSString *key = properties[i];
-//        id value = [view performSelector:NSSelectorFromString(key)];
-        CGRect value = [view frame];
-        [dictionary setValue:[NSValue valueWithCGRect:value] forKey:key];
-    }
-    NSMutableArray *subviews = [NSMutableArray array];
-    for (NSUInteger i = 0; i < view.subviews.count; i++) {
-        [subviews addObject:[self viewInfo:[view.subviews objectAtIndex:i]]];
-    }
-    [dictionary setValue:subviews forKey:@"subviews"];
-    return dictionary;
++ (NSString *)descriptionFromRect:(CGRect)rect {
+    return [NSString stringWithFormat:@"(X: %.1f, Y: %.1f; W: %.1f, H: %.1f)",
+                                      rect.origin.x, rect.origin.y, rect.size.width, rect.size.height];
 }
 
-+ (NSMutableString *)jsonViewInfo:(UIView *)view {
-    NSMutableString *string = [@"{" mutableCopy];
-    NSArray *properties = @[@"frame"];
-    for (NSUInteger i = 0; i < properties.count; i++) {
-        NSString *key = @"frame";
-        NSString *value = NSStringFromCGRect(view.frame);
++ (NSString *)descriptionFromBOOL:(BOOL)arg {
+    return arg ? @"YES" : @"NO";
+}
+
++ (NSString *)valueForKey:(NSString *)key forView:(UIView *)view {
+    if ([key isEqualToString:@"frame"]) {
+        return [self descriptionFromRect:view.frame];
+
+    } else if ([key isEqualToString:@"bounds"]) {
+        return [self descriptionFromRect:view.bounds];
+
+    } else if ([key isEqualToString:@"hidden"]) {
+        return [self descriptionFromBOOL:view.hidden];
+
+    } else if ([key isEqualToString:@"alpha"]) {
+        return [NSString stringWithFormat:@"%.1f", view.alpha];
+
+    } else if ([key isEqualToString:@"opaque"]) {
+        return [self descriptionFromBOOL:view.opaque];;
+
+    } else if ([key isEqualToString:@"clipsToBounds"]) {
+        return [self descriptionFromBOOL:view.clipsToBounds];
+
+    } else if ([key isEqualToString:@"tag"]) {
+        return [NSString stringWithFormat:@"%d", view.tag];
+
+    } else if ([key isEqualToString:@"backgroundColor"]) {
+        return view.backgroundColor.description;
+
+    } else {
+        return @"";
+    }
+}
+
++ (NSMutableString *)JSONDescription:(UIView *)view params:(NSArray *)params {
+    NSMutableString *string = [[NSMutableString alloc] initWithString:@"{"];
+
+    for (NSUInteger i = 0; i < params.count; i++) {
+        NSString *key = [params objectAtIndex:i];
+        NSString *value = [self valueForKey:key forView:view];
+
+        if (!value.length)
+            continue;
 
         [string appendFormat:@"\"%@\":\"%@\"", key, value];
-        if (i != properties.count - 1 || view.subviews.count > 0) {
+        if (i != params.count - 1 || view.subviews.count > 0) {
             [string appendString:@","];
         }
     }
+
     if (view.subviews.count > 0) {
         [string appendString:@"\"subviews\":["];
         for (NSUInteger i = 0; i < view.subviews.count; i++) {
-            [string appendString:[self jsonViewInfo:[view.subviews objectAtIndex:i]]];
+            [string appendString:[self JSONDescription:[view.subviews objectAtIndex:i] params:params]];
             if (i != view.subviews.count - 1) {
                 [string appendString:@","];
             }
